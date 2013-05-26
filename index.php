9239bf06be318b28c8d608da9447680860824434
Thomas Kapoulas <thomas.kapoulas@gmail.com>
2013-05-26 13:20:54 +0300

    added IPv6 support

--- index.php
+++ index.php
@@ -7,30 +7,32 @@
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="el" xml:lang="el">
 <head>
-<META AUTHOR="Periklis Ntanasis a.k.a. Master_ex">
+<META AUTHOR="Periklis Ntanasis a.k.a. Master_ex && Thomas Kapoulas a.k.a. tomkap">
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-<meta name="keywords" content="traceroute ping nslookup foss aueb" />
-<meta name="description" content="free online network tools by foss.aueb" />
+<meta name="keywords" content="traceroute ping nslookup foss teimes ipv4 ipv6" />
+<meta name="description" content="free online network tools by foss.teimes" />
 <meta name="distribution" content="global" />
-<title>foss.aueb.gr &bull; Network Tools</title>
+<title>foss.tesyd.teimes.gr &bull; Network Tools</title>
 <link rel="icon" href="images/favicon.ico" type="image/x-icon" /> 
-<link rel="shortcut icon" href="http://foss.aueb.gr/forum/images/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="http://foss.tesyd.teimes.gr/favicon.ico" type="image/x-icon" />
 </head>
-<center><a href="http://foss.aueb.gr/" target="_blank"><img style="border-style: none" align="left" src="http://foss.aueb.gr/forum/styles/maxthon/imageset/penguin_logo-shadowed.png" /></a><font size=6><b>FOSS AUEB - Network Tools</b></font></center>
-<h5> Κοινότητα Ελεύθερου Λογισμικού και Λογισμικού Ανοιχτού Κώδικα Οικονομικού Πανεπιστημίου Αθηνών<br/>Free and Open Source Software Community of Athens University of Economics and Business</h5>
+<center><a href="http://foss.aueb.gr/" target="_blank"><img style="border-style: none" align="left" src="http://foss.tesyd.teimes.gr/favicon.png" /></a><font size=6><b>FOSS AUEB - Network Tools</b></font></center>
+<h5> Κοινότητα Ελεύθερου Λογισμικού και Λογισμικού Ανοιχτού Κώδικα ΤΕΙ Μεσολογγίου<br/>Free and Open Source Software Community of TEI of Messolonghi</h5>
 </br>
 <body>
 <form name="input" action="index.php" method="get">
 <select name="servise">
 <option value="traceroute">traceroute</option>
+<option value="traceroute6">traceroute (IPv6)</option>
 <option value="ping">ping</option>
+<option value="ping6">ping (IPv6)</option>
 <option value="nslookup">nslookup</option>
 </select>
 IP ADDRESS:
 <input type="text" name="address" />
 <input type="submit" name ="submit" value="Submit" />
 </form> 
-<font size=1>IPv4 address examle : www.google.com or google.com or 209.85.129.99 - don't use 'http://' prefix </font>
+<font size=1>IPv4/IPv6 address example : www.google.com or google.com or 209.85.129.99 or 2a00:1450:4009:804::1003 - don't use 'http://' prefix </font>
 <hr>
 <?php
 if(isset($_GET['submit']))
@@ -52,9 +54,17 @@ if(isset($_GET['submit']))
 	{
 		exec("ping '".escapeshellcmd($address)."' -c 4",$results);
 	}
+	if($servise=="ping6")
+	{
+		exec("ping6 '".escapeshellcmd($address)."' -c 4",$results);
+	}
 	if($servise=="traceroute")
 	{
 		exec("traceroute '".escapeshellcmd($address)."'",$results);
+	}
+        if($servise=="traceroute6")
+	{
+		exec("traceroute6 '".escapeshellcmd($address)."'",$results);
 	}
 	if($servise=="nslookup")
 	{
@@ -73,8 +83,8 @@ if(isset($_GET['submit']))
 }
 ?>
 <center>
-<? readfile("../services.html"); ?><br/>
-<span style="font-family: sans-serif; text-decoration: none; font-size: 10px;">Powered by <a href="http://foss.aueb.gr">foss.aueb.gr</a> <(' )</span><br/>
+<br/>
+<span style="font-family: sans-serif; text-decoration: none; font-size: 10px;">Powered by <a href="http://foss.tesyd.teimes.gr">foss.tesyd.teimes.gr</a> <(' )</span><br/>
 </center>
 </body>
 </html>
