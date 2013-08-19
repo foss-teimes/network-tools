e34c59e211cf1a487235c9cef8645518a72b8d07
charkost <root@charkost.gr>
2013-08-19 21:21:03 +0300

    traceroute output live update

---
+++ traceroute.php
@@ -0,0 +1,23 @@
+<?php
+	$endmsg = "traceroute finised";
+	$dir = "/dev/shm/traceroute/";
+	$tracefile = $dir.$_SERVER[REMOTE_ADDR];
+
+	if($_GET[action] == "start") {
+		foreach (glob($dir."*") as $file) {
+
+		if (filemtime($file) < time() - 86400) {
+		    unlink($file);
+		    }
+		}
+
+		if($_GET[version] != "traceroute" && $_GET[version] != "traceroute6")
+			exit();
+		exec("$_GET[version] '".escapeshellcmd($_GET[address])."' > $tracefile");
+		exec("echo '$endmsg' >> $tracefile");
+	}
+	elseif ($_GET[action] == "display") {
+		$content = nl2br(file_get_contents("/dev/shm/traceroute/$_SERVER[REMOTE_ADDR]"));
+		echo $content;
+	}
+?>
