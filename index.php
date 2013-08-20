843159063f142d1309552cedaea704f941504b3a
charkost <root@charkost.gr>
2013-08-20 14:09:21 +0300

    live stream output support for traceroute and ping

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 /*
  * created by foss.aueb.gr
- * 
+ * extended by foss.tesyd.teimes.gr
+ *
  * Thomas Kapoulas <tomkap@pebkac.gr>
  * Nick Raptis <airscorp@gmail.com>
+ * Charalampos Kostas <root@charkost.gr>
  */
 ?>
 <?php header('Content-Type: text/html; charset=utf-8'); ?>
@@ -11,7 +13,7 @@
 <html lang="el">
 <head>
 <meta charset="UTF-8" />
-<meta name="author" content="Periklis Ntanasis a.k.a. Master_ex &amp;&amp; Thomas Kapoulas a.k.a. tomkap" />
+<meta name="author" content="Periklis Ntanasis a.k.a. Master_ex, Thomas Kapoulas a.k.a. tomkap, Charalampos Kostas a.k.a charkost " />
 <meta name="keywords" content="traceroute ping nslookup foss teimes ipv4 ipv6" />
 <meta name="description" content="free online network tools by foss.teimes" />
 
@@ -28,94 +30,106 @@
 <div id="header" class="clearfix">
 	<div id="site-logo">
 		<a href="https://foss.tesyd.teimes.gr/" title="Home">
-        	<img src="https://foss.tesyd.teimes.gr/sites/default/files/tux-header.png" alt="Home" />
+			<img src="https://foss.tesyd.teimes.gr/sites/default/files/tux-header.png" alt="Home" />
 		</a>
 	</div>
 	<div id="subheader">
-        	<h1>Εργαλεία Δικτύου - Network Tools</h1>
+		<h1>Εργαλεία Δικτύου - Network Tools</h1>
 	</div>
 </div>
 
 <div id="main">
 <div id="input_form">
 <form name="input" action="index.php" method="get"><p>
-    <select name="service">
+	<select name="service">
 <?php
 
-   $services_array = array(
-        "traceroute"  => "traceroute",
-        "ping"        => "ping",
-        "nslookup"    => "nslookup",
-	"whois"       => "whois",
-    );
-    
-    // List options programmatically
-    // output should look like
-    // <option value="ping" selected="selected">ping</option>
-    foreach ($services_array as $s => $v) {
-        echo '    <option value="'.$s.'"';
-        if (isset($_GET['service']) && $s == $_GET['service'])
-            echo ' selected="selected"';
-        echo '>'.$v.'</option>'."\n    ";
-    }
+	$services_array = array(
+		"traceroute"  => "traceroute",
+		"ping"        => "ping",
+		"nslookup"    => "nslookup",
+		"whois"       => "whois",
+	);
+
+	// List options programmatically
+	// output should look like
+	// <option value="ping" selected="selected">ping</option>
+	foreach ($services_array as $s => $v) {
+		echo '<option value="'.$s.'"';
+		if (isset($_GET['service']) && $s == $_GET['service'])
+			echo ' selected="selected"';
+		echo '>'.$v.'</option>'."\n";
+	}
 ?>
-    </select>
-    IP ADDRESS/HOSTNAME:
-    <input type="text" name="address" value="<?php $ip=$_SERVER['REMOTE_ADDR']; echo (isset($_GET['submit']) ? trim($_GET['address']) : $ip); ?>"/>
-    <input type="submit" name ="submit" value="Submit" /></p>
-    <p class="smallfont">IPv4/IPv6 address example : www.google.com or google.com or 209.85.129.99 or 2a00:1450:4009:804::1003 - don't use 'http://' prefix</p>
-</form> 
+	</select>
+	IP ADDRESS/HOSTNAME:
+	<input type="text" name="address" value="<?php $ip=$_SERVER['REMOTE_ADDR']; echo (isset($_GET['submit']) ? trim($_GET['address']) : $ip); ?>"/>
+	<input type="submit" name ="submit" value="Submit" /></p>
+	<p class="smallfont">IPv4/IPv6 address example : www.google.com or google.com or 209.85.129.99 or 2a00:1450:4009:804::1003 - don't use 'http://' prefix</p>
+</form>
 </div> <!-- input form -->
 <div id="response"><p>
 <?php
+function stream_exec($cmd) {
+	$descriptorspec = array(
+		0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+		1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+		2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+	);
+
+	ob_flush();
+	flush();
+
+	$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+	$status = proc_get_status($process);
+
+	while ($status[running]) {
+		echo nl2br(fgets($pipes[1]));
+		ob_flush();
+		flush();
+		usleep(2000);
+		$status = proc_get_status($process);
+	}
+}
+
 if(isset($_GET['submit']))
 {
 	// use of escapeshellcmd - must be enabled
 	// http://php.net/manual/en/function.escapeshellcmd.php
-	// escapes #&;`|*?~<>^()[]{}$\, \x0A and \xFF. ' and " 
-	// are escaped only if they are not paired. 
+	// escapes #&;`|*?~<>^()[]{}$\, \x0A and \xFF. ' and "
+	// are escaped only if they are not paired.
 	$service = trim($_GET['service']);
 	$address = trim($_GET['address']);
-	$results = null;
+
 	if ( (strpos($address,'/')>0) || (strpos($address,'/')===0) ) {
 		echo "Don't be naughty!";
 		exit();
 	}
-	
+
 	elseif ($service=="whois") {
-		exec("whois '".escapeshellcmd($address)."'",$results);
+		echo nl2br( shell_exec("whois '".escapeshellcmd($address)."'") );
 	}
 
 	elseif ($service=="nslookup") {
-		exec("nslookup '".escapeshellcmd($address)."'",$results);   
+		echo nl2br( shell_exec("nslookup '".escapeshellcmd($address)."'") );
 	}
 
 	elseif (strpos($address, ".") > -1) {
 		if($service=="ping") {
-			exec("ping '".escapeshellcmd($address)."' -c 4",$results);
+			stream_exec("ping '".escapeshellcmd($address)."' -c 4");
 		}
-        
+
 		elseif ($service=="traceroute") {
-			exec("traceroute '".escapeshellcmd($address)."'",$results);
+			stream_exec("traceroute '".escapeshellcmd($address)."'");
 		}
 	}
-
 	else {
 		if($service=="ping") {
-			exec("ping6 '".escapeshellcmd($address)."' -c 4",$results);
+			stream_exec("ping6 '".escapeshellcmd($address)."' -c 4");
 		}
 		elseif($service=="traceroute") {
-			exec("traceroute6 '".escapeshellcmd($address)."'",$results);
+			stream_exec("traceroute6 '".escapeshellcmd($address)."'");
 		}
-	}
-	
-	foreach ($results as $result) {
-		echo $result;
-		echo "<br />\n";
-	}
-	
-	if ($results == null) {
-		echo "Address format error or address doesn't exist";
 	}
 }
 ?>
